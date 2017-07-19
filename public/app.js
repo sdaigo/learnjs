7f477fa4d674b1e38078e88a4d2a112d11bfb794
Daigo Shitara <daigo@marcelle.cc>
2017-07-20 00:07:55 +0900

    :sparkles: add routing

--- public/app.js
+++ public/app.js
@@ -3,7 +3,28 @@
 // app's namespace
 var learnjs = {};
 
+learnjs.appOnReady = function() {
+  window.onhashchange = function() {
+    learnjs.showView(window.location.hash);
+  }
+
+  learnjs.showView(window.location.hash);
+}
+
+learnjs.problemView = function(problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming Soon!';
+  return $('<div class="problem-view">').text(title);
+};
+
 learnjs.showView = function (hash) {
-  var probremView = $('<div class="probrem-view">').text('Coming Soon!');
-  $('.view-container').empty().append(probremView);
+  var routes = {
+    '#problem': learnjs.problemView,
+  };
+
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
 };
